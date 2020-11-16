a468a81e5cddab5222de4f696a402781ce102572
Irina <igiosanu29@gmail.com>
2020-11-16 15:01:46 +0000

    Updated Fizzbuzz code

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,13 @@
-def fizzbuzz(number)
-  "fizz"
+def fizzbuzz(n)
+  (1..30).each do |n|
+    if (n % 3 == 0) && (n % 5 == 0) 
+      puts "FizzBuzz"
+    elsif n % 3 == 0
+      puts "Fizz"
+    elsif n % 5 == 0
+      puts "Buzz"
+    else 
+      puts n
+    end
+  end
 end
